0c5f673b406bd6782a725d647948993d43c6351b
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2020-11-01 11:09:42 +0100

    OpenAi: mountain car -- code from net example -- working

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-import example_test
-import mountain_car
 
 
-import time
-import numpy as np
-import matplotlib.pyplot as plt
-import gym
-import random
+import OpenAi.CartPole.example_test
 
+import OpenAi.MountainCar.example as MountainCar_example
+
+
+
+
+
+
+
+
+
+##      ______CART POLE______
 
 #example_test.cart_pole_v1()
 
 
+##      ______MOUNTAIN CAR______
 
+#mountain_car.test_play_display_info()
 
-mountain_car.test_play_display_info()
+print("[+] Playing Random Games")
+df = MountainCar_example.play_random_games(games=1000)
 
+print("[+] Training NN Model")
+ml_model = MountainCar_example.generate_ml(df)
+
+print("[+] Playing Games with NN")
+MountainCar_example.play_game(ml_model=ml_model, games=3)
