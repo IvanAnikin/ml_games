502c9809ba5a31df94050438f8bb948e3240654d
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2020-12-04 09:03:03 +0100

    testin

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ stats_every = 40000
 
 #MountainCar_Q_Learning_gym_agent.save_games(LEARNING_RATES=LEARNING_RATES, EPSILONS= EPSILONS, END_EPSILON_DECAYING_POSITIONS=END_EPSILON_DECAYING_POSITIONS, DISCOUNTS=DISCOUNTS, DISCRETE_OS_SIZES=DISCRETE_OS_SIZES, episodes=episodes, show_every=show_every, stats_every=stats_every)
 
-MountainCar_Q_Learning_visualisation_agent.visualise_stats(LEARNING_RATES=LEARNING_RATES, EPSILONS= EPSILONS, END_EPSILON_DECAYING_POSITIONS=END_EPSILON_DECAYING_POSITIONS, DISCOUNTS=DISCOUNTS, DISCRETE_OS_SIZES=DISCRETE_OS_SIZES, episodes=episodes, show_every=show_every, stats_every=stats_every)
+#MountainCar_Q_Learning_visualisation_agent.visualise_stats(LEARNING_RATES=LEARNING_RATES, EPSILONS= EPSILONS, END_EPSILON_DECAYING_POSITIONS=END_EPSILON_DECAYING_POSITIONS, DISCOUNTS=DISCOUNTS, DISCRETE_OS_SIZES=DISCRETE_OS_SIZES, episodes=episodes, show_every=show_every, stats_every=stats_every)
 
 #print(MountainCar_Q_Learning_stats_agent.best_rewards_params(score_count_start_position=9000, LEARNING_RATES=LEARNING_RATES, EPSILONS= EPSILONS, END_EPSILON_DECAYING_POSITIONS=END_EPSILON_DECAYING_POSITIONS, DISCOUNTS=DISCOUNTS, DISCRETE_OS_SIZES=DISCRETE_OS_SIZES, episodes=episodes, show_every=show_every, stats_every=stats_every))
 
@@ -47,10 +47,10 @@ MountainCar_Q_Learning_visualisation_agent.visualise_stats(LEARNING_RATES=LEARNI
 
 #MountainCar_Q_Learning_storage_agent.save_np("q_table_30000__lr-0.2__eps-0.7__epsDec-1.5__disc-0.95__size-[20, 20]", q_table)
 
-#q_table = MountainCar_Q_Learning_storage_agent.load_np("q_table_30000__lr-0.2__eps-0.7__epsDec-1.5__disc-0.95__size-[20, 20]")
-#successful_episodes, average_score = MountainCar_Q_Learning_gym_agent.play_with_given_q_table(q_table, [20, 20], 1000, 1)
-#print("successful_episodes: ", successful_episodes)
-#print("average_score: ", average_score)
+q_table = MountainCar_Q_Learning_storage_agent.load_np("q_table_30000__lr-0.2__eps-0.7__epsDec-1.5__disc-0.95__size-[20, 20]")
+successful_episodes, average_score = MountainCar_Q_Learning_gym_agent.play_with_given_q_table(q_table, [20, 20], 2000, 1)
+print("successful_episodes: ", successful_episodes)
+print("average_score: ", average_score)
 
 
 
